691b764283bbf00cedea4d96b693d3e719436084
Sahil Mhatre <34106474+sahilvinodMhatre@users.noreply.github.com>
2024-07-27 00:12:52 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Namaste 🙏",
+        message: "Namaste everyone 🙏",
       },
       null,
       2
